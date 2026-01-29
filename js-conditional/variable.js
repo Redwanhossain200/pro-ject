def10b6f5ece5dd3b385ee19fa0d0be38e95a165
Redwanhossain200 <mdredwanhossain2007@gmail.com>
2026-01-29 11:27:07 +0600

    added variable.js

---
+++ js-conditional/variable.js
@@ -0,0 +1,33 @@
+/* VARIABALE ==> Container */
+
+// Number type variable 
+var sunglassPrice = 80;
+console.log(sunglassPrice);
+
+// String 
+var subject = 'CSE';
+console.log(subject);
+
+// Boolean 
+var passed = true;
+console.log(passed);
+
+/* Naming convention 
+1. single word
+2. no quote
+3. no gap or dash
+4. can not start with number
+5. cannot be a keyword
+6. Prefer camelCase*/
+
+var lastExamScore = 89;
+console.log(lastExamScore);
+
+
+/* OPERATION 
+1. + , - , * , /
+2.+= , -= , *= , /=
+3.isNaN ?
+4.null
+5.Number
+*/
