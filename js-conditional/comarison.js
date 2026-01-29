5c43e8fed575e3822d35f0d47af29f0090402db6
Redwanhossain200 <mdredwanhossain2007@gmail.com>
2026-01-29 12:42:16 +0600

    add file

---
+++ js-conditional/comarison.js
@@ -0,0 +1,32 @@
+// console.log(5 > 10);
+// console.log(5 < 10);
+// console.log(5 == 10);
+// console.log(10 == 10);
+// console.log(15 >= 10);
+// console.log(5 <= 10);
+// console.log(10 <= 10);
+// console.log(100 <= 10);
+
+// console.log(10 == 10);
+// console.log(10 == 10.1);
+
+// -------------------
+// var payaraPrice = 40;
+// var applePrice = 250;
+// compair= payaraPrice <= applePrice;
+// console.log(compair);
+
+/**
+ * ==
+ * ===
+ * 
+*/
+
+// console.log(10 == '10');
+// console.log(10 === '10');
+
+// console.log(120 === 'tomato');
+
+// -------------------------------
+console.log(10 != '10');
+console.log(10 !== '10');
